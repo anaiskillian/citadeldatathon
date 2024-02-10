a329261c817619a38009c7a023ea3032c2971062
Anais Killian <anaiskillian@college.harvard.edu>
2024-02-10 00:25:20 -0500

    example

--- comp.py
+++ comp.py
@@ -1,1 +1,20 @@
+# Imports needed across sections
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import folium
+import seaborn as sns
+import nltk
+from nltk.tokenize import word_tokenize
+from collections import Counter
+from nltk.probability import FreqDist
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.naive_bayes import MultinomialNB
+from tabulate import tabulate
+from IPython.display import display, HTML
+
 
